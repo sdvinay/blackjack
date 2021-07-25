abc596601e0e1daf346bc0d235347a43b57347c2
Vinay Kumar <sdvinay@gmail.com>
2021-07-24 20:07:49 -0700

    Modify the iterative optimizer to keep going as long as it keeps finding improvements

--- optimizer.py
+++ optimizer.py
@@ -18,12 +18,16 @@ def run_iteration(n, strat_base, previous_instructions):
     # Visualize the average outcome by starting condition
     sns.heatmap(outputs['winning_act_outcome'].unstack(), ax=fig.add_subplot(1, 2, 2))
     
+    # Identify changes in this iteration
+    iter_changed = True
     if previous_instructions:
+        iter_changed = False
         for k in instructions:
             if instructions[k] != previous_instructions[k]:
+                iter_changed = True
                 print(k, instructions[k], previous_instructions[k])
     
-    return instructions
+    return (iter_changed, instructions)
 
 # Memoized strategy
 # Rather than generic conditions, just use an array indexed on player and dealer scores
@@ -48,19 +52,21 @@ class Strat_memoized(bj.Strategy):
 def gen_strat_memoized(instructions, strat_base):
     return Strat_memoized(strat_base, instructions)
 
-def derive_iterative_strategies(strat_base, iterations):
+def derive_iterative_strategies(strat_base, max_iterations):
     n = 50
     previous_instructions = None
     strategies = [strat_base]
 
-    for i in range(iterations):
-        print(i)
-        instructions = run_iteration(n, strat_base, previous_instructions)
+    for i in range(max_iterations):
+        print(f'Starting iteration {i}')
+        iter_changed, instructions = run_iteration(n, strat_base, previous_instructions)
         strat_new  = gen_strat_memoized(instructions, strat_base)
         strat_new.name = f'iter({i+1})'
         strategies.append(strat_new)
         previous_instructions = instructions
         strat_base = strat_new
-        n = n*3
+        n = n*2
+        if not iter_changed:
+            break
 
     return strategies
