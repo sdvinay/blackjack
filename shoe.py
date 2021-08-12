2208d0bc200ac6ed77521f411f71fe41fe2bd4de
Vinay Kumar <sdvinay@gmail.com>
2021-08-11 18:38:40 -0700

    Stateful shoe: make the internal __shoe private, and make cards() available

--- shoe.py
+++ shoe.py
@@ -1,5 +1,8 @@
-import blackjack as bj
 import random
+from typing import Sequence
+
+import blackjack as bj
+
 
 # Models a real-life multi-deck shoe, which is re-shuffled periodically
 # Implemented by creating a DisposableShoe on every shuffle(), and dealing from that DisposableShoe
@@ -7,7 +10,7 @@ import random
 
 class StatefulShoe(bj.Shoe):
     num_decks = 8
-    shoe: bj.DisposableShoe
+    __shoe: bj.DisposableShoe
     def __init__(self) -> None:
         super().__init__()
         self.shuffle()
@@ -15,7 +18,10 @@ class StatefulShoe(bj.Shoe):
     def shuffle(self) -> None:
         cards = list(range(1, 14))*4*self.num_decks
         random.shuffle(cards)
-        self.shoe = bj.DisposableShoe(cards)
+        self.__shoe = bj.DisposableShoe(cards)
 
     def deal(self) -> bj.Card:
-        return self.shoe.deal()
+        return self.__shoe.deal()
+
+    def cards(self) -> Sequence[bj.Card]:
+        return self.__shoe.cards
